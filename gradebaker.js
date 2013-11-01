e308131f1064b2a403b94873d2ba82c05e82af19
Alice Nguyen <a8nguyen@ucsd.edu>
2013-10-31 23:10:07 -0700

    Input fields working for updating and creating category labels. Input fields for weight gets generated but has no behavior as of yet. Also spiced things up a bit by adding some color and font, for now.

--- gradebaker.js
+++ gradebaker.js
@@ -9,14 +9,14 @@ var syllabusModule = (function () {
             var value = defaultItem[i][1];
             category[name] = { 
                 weight : value,
-                tag : (function(){_createTag(name, value)})
+    tag : (function(){_createTag(name, value)})
             }
             _addInputField(name, value);
         }
     }
 
     function _createTag (name, weight) {
-        return '<tr class="category_input"><td><p>' + name + '</p></td><td><input type="text" name='+name+' value='+ weight +' onchange="syllabusModule.setWeight(this.name,this.value)"></td></tr>';
+        return '<tr class="category_input" name='+name+'><td><input type="text" name="label" value='+name+'></td><td><input type="text" name="weight" value='+ weight +' onchange="syllabusModule.setWeight(this.name,this.value)"></td></tr>';
     }
 
     function _addInputField (name, weight) {
@@ -28,9 +28,9 @@ var syllabusModule = (function () {
 
     /* public properties */
     return {
-            init : (function () {
-                _init();
-            })(),
+        init : (function () {
+            _init();
+        })(),
 
              addCategory : function (name) {   // values is [name, weight]
                  category[name] = { weight : 0 };
@@ -42,39 +42,89 @@ var syllabusModule = (function () {
              },
 
              setWeight : function (name, newValue) {
-                console.log("testing setWeight: " + name + ', ' + newValue);
-                category[name].weight = newValue;
-                console.log("post setWeight: category[" +name + "] weight=" + category[name].weight );
+                 console.log("testing setWeight: " + name + ', ' + newValue);
+                 category[name].weight = newValue;
+                 console.log("post setWeight: category[" +name + "] weight=" + category[name].weight );
+             },
+
+             setName : function (oldName, newName) {
+                 if (category.hasOwnProperty(oldName)) {
+                     category[newName] = category[oldName];
+                     delete category[oldName];
+                     return true;
+                 }
+                 return false;
+             },
+
+             hasCategory : function (name) {
+                 return category.hasOwnProperty(name);
              },
 
              /* debugging functions */
              printAll : function () {
-                var i = 0;
-                for ( k in category) {
-                    console.log("("+ i++ +"): " + k + ", " + category[k].weight);
+                 var i = 0;
+                 for ( k in category) {
+                     console.log("("+ i++ +"): " + k + ", " + category[k].weight);
 
-                }
+                 }
              }
     };
 
-}());
+})();
+
+console.log(syllabusModule.hasCategory("assignments"));
 //var tr =  '<tr class="category_input"><td><input type="text" name="name"></td><td><input type="text" name="weight"></td></tr>';
 /* Event funtions */
 function addCategoryField() {
-    var tr =  '<tr class="category_input"><td><input type="text" onchange="categoryOnChange(this)"></td></tr>'
+    var tr =  '<tr class="category_input" name><td><input type="text" name="label" value onchange="categoryOnChange(this)"></td></tr>';
     var tableNode = document.getElementById("input_table");
     tableNode.innerHTML += tr;
+    console.log(tableNode.innerHTML);
 }
 
-function addWeightField(cursor) {
-    var trW = '<td><input type="text" name="weight"></td>';
-    cursor.innterHTML += trW;
+function hasWeightField(trNode) {
+
 }
+function addWeightField(trNode) {
+    var tr_weight = '<td><input type="text" name="weight" value="0"></td>';
+    trNode.innerHTML += tr_weight;
+    _print(trNode.innerHTML); // DEBUG
+}
+
 /* insert category into syllabusModule and create input field for weight after the user enters in a category*/
-function categoryOnChange (node) {
-    // update the name attribute in <tr> tag
-    node.name = node.value;
-    console.log("node update: " + node.name);
-    syllabusModule.addCategory(node.name);
+function categoryOnChange (e) {
+    var newName = e.value;
+    var trNode = e.parentNode.parentNode;
+    var labelNode = e;
+    /* empty str input */
+    if (isEmpty(newName))
+        return;
+
+    labelNode.setAttribute("value", newName);
+
+    /* create brand new category & create weight field */
+    if (!syllabusModule.hasCategory(newName) && trNode.childNodes.length < 2) {
+        syllabusModule.addCategory(newName);
+        addWeightField(trNode);
+        _print(labelNode.innerHTML); // DEBUG
+    }
+    /* update existing category name only */
+    else { 
+        syllabusModule.setName(e.name, newName); 
+
+    }
+    trNode.setAttribute("name",newName);
+    syllabusModule.printAll();
 }
 
+function isEmpty(k) {
+    return (k.length == 0);
+}
+
+function _print(x) {
+    console.log("_print(): " + x + ': ' + x);
+}
+/*
+   Array.prototype.slice.call(document.getElementsByName(name)[0].attributes).forEach(function(item) {
+   console.log("attrs:  " + item.name + ': '+ item.value);
+   });*/
